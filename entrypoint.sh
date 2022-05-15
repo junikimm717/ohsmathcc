5c8a81fb4e8697c61fb87b84ddc8bb4820566b7e
junikimm717 <68165832+junikimm717@users.noreply.github.com>
2022-05-15 14:30:51 -0400

    increase workers to 10

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@
 python manage.py migrate auth || exit 1
 python manage.py migrate || exit 1
 python manage.py collectstatic || exit 1
-hypercorn -w 3 -b 0.0.0.0:8000 clubapp.asgi:application
+hypercorn -w 10 -b 0.0.0.0:8000 clubapp.asgi:application
