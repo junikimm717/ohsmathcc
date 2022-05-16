4e2be1f3f849f844b10b0b9da30f85244bdf3ebc
junikimm717 <68165832+junikimm717@users.noreply.github.com>
2022-05-15 22:41:06 -0400

    uniformity of border radius

--- dev-db.sh
+++ dev-db.sh
@@ -2,8 +2,14 @@
 
 DIR="$(realpath "$(dirname "$0")")"
 
+cd "$DIR" || exit 1
+
+
 case "$1" in
   up|u)
+    if test -f "$DIR/docker-compose.yml"; then
+      docker-compose down
+    fi
     docker-compose -f "$DIR/docker-compose.dev.yml" up -d
     ;;
   down|d)
